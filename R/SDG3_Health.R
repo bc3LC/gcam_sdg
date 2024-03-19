5e0303862ad6bd615dc5df1ae970716ee68137cb
Jon Sampedro <jon.sampedro@bc3.net>
2024-03-19 13:09:18 +0100

    update queries for new gcam structure

--- R/SDG3_Health.R
+++ R/SDG3_Health.R
@@ -14,10 +14,10 @@ get_sdg3_health <- function(prj, final_db_year = 2050){
   if (!dir.exists("output/SDG3-Health/figures")) dir.create("output/SDG3-Health/figures")
   if (!dir.exists("output/SDG3-Health/maps")) dir.create("output/SDG3-Health/maps")
   
-  scen <- rgcam::listScenarios(prj)
+  scen_name <- rgcam::listScenarios(prj)
   
   mort <- rfasst::m3_get_mort_pm25(prj_name = prj,
-                   scen_name = scen,
+                   scen_name = scen_name,
                    final_db_year = final_db_year,
                    saveOutput = F)
   
