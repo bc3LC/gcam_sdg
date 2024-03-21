00d6c98261198f14f2de948b9668872ad6331cff
Jon Sampedro <jon.sampedro@bc3.net>
2024-03-21 13:55:43 +0100

    organize load prj

--- R/load_prj.R
+++ R/load_prj.R
@@ -4,8 +4,7 @@ library(rgcam)
 library(gcamdata)
 
 load_prj <- function(db_path = NULL, query_path = "./inst/extdata", db_name = NULL, prj_name,
-                rdata_name = NULL, scen_name, queries = "queries_rfasst.xml", final_db_year = 2100, 
-                ssp = "SSP2", recompute = F){
+                rdata_name = NULL, scen_name, final_db_year = 2100, recompute = F){
   
   print('loading project...')
   
@@ -13,29 +12,38 @@ load_prj <- function(db_path = NULL, query_path = "./inst/extdata", db_name = NU
   #----------------------------------------------------------------------
   # Assert that the parameters of the function are okay, or modify when necessary
   
-  if (!endsWith(prj_name, '.dat')) prj_name = paste0(prj_name, '.dat')
+  # if (!endsWith(prj_name, '.dat')) prj_name = paste0(prj_name, '.dat')
   
   #----------------------------------------------------------------------
   #----------------------------------------------------------------------
+  # Load project file for SDG1
+  # prj_sdg1 <- 
   
-  # Load the rgcam project:
+  #----------------------------------------------------------------------
+  # Load project file for SDG2
+  # prj_sdg2 <- 
+  
+  #----------------------------------------------------------------------
+  # Load project file for SDG3: Requires some changes for nonCO2 emissions (already solved in rfasst package)
+
+    # Load the rgcam project:
   if (!is.null(db_path) & !is.null(db_name)) {
     rlang::inform('Creating project ...')
     conn <- rgcam::localDBConn(db_path,
                                db_name,migabble = FALSE)
-    prj <- rgcam::addScenario(conn,
+    prj_sdg3 <- rgcam::addScenario(conn,
                               prj_name,
                               scen_name,
                               paste0(query_path,"/",queries),
                               saveProj = F)
-    prj <- fill_queries(prj, db_path, db_name, prj_name, scen_name)
+    prj_sdg3 <- fill_queries(prj_sdg3, db_path, db_name, prj_name, scen_name)
     
-    rgcam::saveProject(prj, file = file.path('output',prj_name))
+    rgcam::saveProject(prj_sdg3, file = file.path('output',prj_name))
     
-    QUERY_LIST <- c(rgcam::listQueries(prj, c(scen_name)))
+    QUERY_LIST <- c(rgcam::listQueries(prj_sdg3, c(scen_name)))
   } else if (is.null(rdata_name)){
     rlang::inform('Loading project ...')
-    prj <- rgcam::loadProject(prj_name)
+    prj_sdg3 <- rgcam::loadProject(prj_name)
     
     QUERY_LIST <- c(rgcam::listQueries(prj, c(scen_name)))
   } else {
@@ -46,5 +54,15 @@ load_prj <- function(db_path = NULL, query_path = "./inst/extdata", db_name = NU
     }
   }
   
+  #----------------------------------------------------------------------
+  # Load project file for SDGK
+  # prj_sdg_k <- 
 
+  
+  #----------------------------------------------------------------------
+  #----------------------------------------------------------------------
+  # Merge projects!
+  # prj <- rgcam::mergeProjects()
+  
+  
 } 
